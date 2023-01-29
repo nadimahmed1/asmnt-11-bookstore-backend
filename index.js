b77b5780fd93ca7cf39a6eb0c845112482d30679
nadimahmed1 <nadimarafat26@gmail.com>
2023-01-29 22:56:23 +0600

    api added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 // 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chw5ad1.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER_BOOK}:${process.env.DB_PASS_BOOK}@cluster0.chw5ad1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log('db connected');
 
@@ -28,6 +28,23 @@ console.log('db connected');
 async function run() {
     try {
         await client.connect()
+
+        const storeCollection = client.db('bicycle').collection('store')
+        const bicycleCollection2 = client.db('bicycle2').collection('store2')
+
+        app.get('/bicycle', async (req, res) => {
+            const query = {};
+            const cursor = storeCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/bicycle2', async (req, res) => {
+            const query = {};
+            const cursor = bicycleCollection2.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
     }
     catch {
 
